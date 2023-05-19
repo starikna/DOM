908cf61abe35da1216744fb6d601d3c9090cace3
starikna <don.strekoza@gmail.com>
2023-05-19 22:51:48 +0500

    Second commit

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -3,7 +3,6 @@ import App from "../app";
 test("test create div class 'hole'", () => {
   const app = new App(4);
   const expected = `<div class="hole" id="hole1"></div>`;
-  const received = App.getHole(1);
 
   expect(app.getHole(1)).toEqual(expected);
 });
