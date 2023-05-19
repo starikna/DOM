26565fd6d09139a9f7026aa78047fab518c84ef6
starikna <don.strekoza@gmail.com>
2023-05-19 22:49:28 +0500

    Second commit

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -1,10 +1,9 @@
 import App from "../app";
 
-test("test create div class 'goblin'", () => {
-  App.getHole(1);
+test("test create div class 'hole'", () => {
+  const app = new App(4);
+  const expected = `<div class="hole" id="hole1"></div>`;
+  const received = App.getHole(1);
 
-  const expected = `<div class="hole" id="hole1"><div class="goblin"</div></div>`;
-
-  const received = toString(App.getHole(1));
-  expect(received).toEqual(expected);
+  expect(app.getHole(1)).toEqual(expected);
 });
