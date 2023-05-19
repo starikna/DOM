c89b1576152b1382ea9f9801dbf3a6e15490706e
starikna <don.strekoza@gmail.com>
2023-05-19 22:42:33 +0500

    Second commit

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -3,8 +3,8 @@ import App from "../app";
 test("test create div class 'goblin'", () => {
   App.getHole(1);
 
-  const expected = `<div class="hole" id="hole1"></div>`;
+  const expected = `<div class="hole" id="hole1"><div class="goblin"</div></div>`;
 
-  const received = App.getHole(1);
+  const received = toString(App.getHole(1));
   expect(received).toEqual(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -25,6 +25,7 @@ export default class App {
   }
 
   static getHole(index) {
+    console.log(document.getElementById(`hole${index}`));
     return document.getElementById(`hole${index}`);
   }
 
